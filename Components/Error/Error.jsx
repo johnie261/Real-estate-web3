f2b42dd2b912401c00a424346e5f526795cb0952
johnie261 <njorogejohn261@gmail.com>
2023-09-07 19:24:12 +0300

    Add Error Component

--- Components/Error/Error.jsx
+++ Components/Error/Error.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Style from './Error.module.css'
 
-const Error = () => {
+const Error = ({error}) => {
   return (
-    <div>Error</div>
+    <div className={Style.Error}>
+      <div className={Style.Error_box}>
+        <h1>Please fix this error and reload browser</h1>
+        {error}
+      </div>
+    </div>
   )
 }
 
